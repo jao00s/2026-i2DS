c280c82d39de8b2adf0d41f807bfc9e618e1f33e
jao00s <joaofcojau@gmail.com>
2026-08-04 16:57:59 -0300

    exercicio02

--- Aula-03/atividades/Exercicio02/Program.cs
+++ Aula-03/atividades/Exercicio02/Program.cs
@@ -2,6 +2,41 @@
 {
     private static void Main(string[] args)
     {
+        double num1;
+        double num2;
+
+        Console.Write("Digite o Primeiro Número: ");
+        num1 = double.Parse(Console.ReadLine());
         
-    }
+         Console.Write("Digite o segundo Número: ");
+        num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+
+        string operação
+
+        case "1":
+        Console.WriteLine(num1 + num2);
+            break;
+
+            case "2":
+            Console.WriteLine(num1 - num2);
+            break;
+
+            case "3":
+            Console.WriteLine(num1 * num2);
+            break;
+
+            case "4":
+            Console.WriteLine(num1 / num2);
+            break;
+            default:
+                Console.WriteLine("Operador informado nao é valido");
+                break;
+
+
+
 }
