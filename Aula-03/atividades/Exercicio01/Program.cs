f4b37d1b18cab8a22f6ef08bf7139747eccbea06
jao00s <joaofcojau@gmail.com>
2026-08-04 16:44:32 -0300

    exercicio 01

--- Aula-03/atividades/Exercicio01/Program.cs
+++ Aula-03/atividades/Exercicio01/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        double celsius;
+        double fahrenheit;
+
+        Console.Write("Digite a Temperatua em celsius: ");
+        celsius = double.Parse(Console.ReadLine());
+
+        fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("graus celsius equivalem a " + fahrenheit + " graus a fahrenheit");
+    } 
 }
